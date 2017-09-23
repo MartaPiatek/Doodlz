670d8add01ff14c2763848fa26a44a2b78ad1d92
MartaPiatek <martusia.piatek@gmail.com>
2017-09-23 19:16:17 +0200

    Ustawienie orientacji ekranu aplikacji

--- app/src/main/java/pl/martapiatek/doodlz/MainActivity.java
+++ app/src/main/java/pl/martapiatek/doodlz/MainActivity.java
@@ -1,4 +1,6 @@
 package pl.martapiatek.doodlz;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,7 +16,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //określa rozmiar okna
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
 
+        // duze tablety - orientacja pozioma, pozostale urzadzenia - orientacja pionowa
+        if(screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        else
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
 
