31202cb2052d3e4994d7bb240447152638217f0e
MartaPiatek <martusia.piatek@gmail.com>
2017-09-23 16:39:49 +0200

    Dodanie widoku DoodleView

---
+++ app/src/main/java/pl/martapiatek/doodlz/DoodleView.java
@@ -0,0 +1,18 @@
+package pl.martapiatek.doodlz;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Marta on 23.09.2017.
+ */
+
+public class DoodleView extends View {
+
+
+    public DoodleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
